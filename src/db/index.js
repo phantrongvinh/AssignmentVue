6bca160af227eeba2eeff99591e81deb4f406b11
PhanTrongVinh <1phantrongvinh98@gmail.com>
2026-02-22 22:38:57 +0700

    Vue

--- src/db/index.js
+++ src/db/index.js
@@ -116,7 +116,7 @@ const blogs = ref([
     authorID: 1,
     createdAt: new Date(),
     img: "new-day.jpg",
-    likes: [],
+    likes: [3],
   },
   {
     blogID: 3,
@@ -128,10 +128,116 @@ const blogs = ref([
       "Thế bản chất cuộc sống là gì? Đối với tác giả, đó là cuộc đời không cô độc. Mỗi cá thể luôn sở hữu một nửa trái tim về mặt tinh thần, luôn có một nửa còn lại đang ở đâu đó trên thế gian đợi ta phát hiện. Thế cuộc sống có phải là tình yêu? Cũng có thể và cũng có thể nói tình yêu là một phần của việc được 'sống'. Ta phải làm mọi thứ dể bản thân tồn tại trên thế gian nhưng trải qua những điều để bồi dưỡng cảm xúc mới gọi là được 'sống'. Yêu và được yêu có thể nói rằng ta đã được 'sống' hơn nửa đời người.",
       "Ta được 'sống' , được yêu, đó được gọi là một cuộc đời bình yên. Ta được là chính ta, ta có nơi quay về, đó là mái ấm, không phải là nơi chốn mà là một người ta gọi là yêu thương.",
     ],
-    authorID: 2,
+    authorID: 3,
     createdAt: new Date(),
     img: "regret.jpg",
-    likes: [1, 2, 4, 5, 7, 9, 10],
+    likes: [3, 4, 7],
+  },
+  {
+    blogID: 4,
+    title: "Con cái đã 'dạy' gì cho cha mẹ",
+    description:
+      "Chúng ta mỗi ngày đều học cách trưởng thành hơn qua kinh nghiệm và trải nghiệm, đến một ngày được lại cha mẹ thì có dễ dàng hơn chăng",
+    contents: [
+      "Đi qua tuổi trẻ, ta tìm kiếm nơi náo nhiệt. Năng lượng thuần khiét của tuổi dậy thì thúc đẩy ta trải qua những điều ta chưa bao giờ dám. Sau khi nếm đủ hương vị của cuộc sống, những khó khăn và uất ức cũng lần lượt xuất hiện. Thế nên bình yên lại là điều cuối cùng ta muốn có được. Bình yên là gì, là cảm giác được yên lòng, là không cần bận tâm những điều xung quanh, là mỗi ngày đều không có gánh nặng trên vai, là nụ cười trên môi, là những điều mình muốn luôn xuất hiện xung quanh,... Thật ra mỗi người đều có một cái gọi là bình yên, có khi giống nhau cũng có khi giống nhau. Nhưng lại đúc kết ra bình yêu là điều bản thân mong muốn nhất về một cuộc sống.",
+      "Thế bản chất cuộc sống là gì? Đối với tác giả, đó là cuộc đời không cô độc. Mỗi cá thể luôn sở hữu một nửa trái tim về mặt tinh thần, luôn có một nửa còn lại đang ở đâu đó trên thế gian đợi ta phát hiện. Thế cuộc sống có phải là tình yêu? Cũng có thể và cũng có thể nói tình yêu là một phần của việc được 'sống'. Ta phải làm mọi thứ dể bản thân tồn tại trên thế gian nhưng trải qua những điều để bồi dưỡng cảm xúc mới gọi là được 'sống'. Yêu và được yêu có thể nói rằng ta đã được 'sống' hơn nửa đời người.",
+      "Ta được 'sống' , được yêu, đó được gọi là một cuộc đời bình yên. Ta được là chính ta, ta có nơi quay về, đó là mái ấm, không phải là nơi chốn mà là một người ta gọi là yêu thương.",
+    ],
+    authorID: 6,
+    createdAt: new Date(),
+    img: "family.jpg",
+    likes: [1, 2, 4, 6, 7],
+  },
+  {
+    blogID: 5,
+    title: "Chúng ta chỉ 'chết' đi khi không còn ước mơ",
+    description:
+      "Cả đời người chúng ta cố gắng để đạt thứ gọi là ước mơ, là điều chúng ta luôn nhắm đến khi bắt đầu có nhận thức",
+    contents: [
+      "Đi qua tuổi trẻ, ta tìm kiếm nơi náo nhiệt. Năng lượng thuần khiét của tuổi dậy thì thúc đẩy ta trải qua những điều ta chưa bao giờ dám. Sau khi nếm đủ hương vị của cuộc sống, những khó khăn và uất ức cũng lần lượt xuất hiện. Thế nên bình yên lại là điều cuối cùng ta muốn có được. Bình yên là gì, là cảm giác được yên lòng, là không cần bận tâm những điều xung quanh, là mỗi ngày đều không có gánh nặng trên vai, là nụ cười trên môi, là những điều mình muốn luôn xuất hiện xung quanh,... Thật ra mỗi người đều có một cái gọi là bình yên, có khi giống nhau cũng có khi giống nhau. Nhưng lại đúc kết ra bình yêu là điều bản thân mong muốn nhất về một cuộc sống.",
+      "Thế bản chất cuộc sống là gì? Đối với tác giả, đó là cuộc đời không cô độc. Mỗi cá thể luôn sở hữu một nửa trái tim về mặt tinh thần, luôn có một nửa còn lại đang ở đâu đó trên thế gian đợi ta phát hiện. Thế cuộc sống có phải là tình yêu? Cũng có thể và cũng có thể nói tình yêu là một phần của việc được 'sống'. Ta phải làm mọi thứ dể bản thân tồn tại trên thế gian nhưng trải qua những điều để bồi dưỡng cảm xúc mới gọi là được 'sống'. Yêu và được yêu có thể nói rằng ta đã được 'sống' hơn nửa đời người.",
+      "Ta được 'sống' , được yêu, đó được gọi là một cuộc đời bình yên. Ta được là chính ta, ta có nơi quay về, đó là mái ấm, không phải là nơi chốn mà là một người ta gọi là yêu thương.",
+    ],
+    authorID: 2,
+    createdAt: new Date(),
+    img: "dream.jpg",
+    likes: [3, 6],
+  },
+  {
+    blogID: 6,
+    title:
+      "Quá khứ quyết định hiện tại, hiện tại quyết định tương lai, và tương lai chấp cánh cho hiện tại",
+    description:
+      "Các cột mốc, thời điểm, khoảng thời gian vòng đời người đều liên quan và bổ trợ nhau như phần không thể thiếu",
+    contents: [
+      "Đi qua tuổi trẻ, ta tìm kiếm nơi náo nhiệt. Năng lượng thuần khiét của tuổi dậy thì thúc đẩy ta trải qua những điều ta chưa bao giờ dám. Sau khi nếm đủ hương vị của cuộc sống, những khó khăn và uất ức cũng lần lượt xuất hiện. Thế nên bình yên lại là điều cuối cùng ta muốn có được. Bình yên là gì, là cảm giác được yên lòng, là không cần bận tâm những điều xung quanh, là mỗi ngày đều không có gánh nặng trên vai, là nụ cười trên môi, là những điều mình muốn luôn xuất hiện xung quanh,... Thật ra mỗi người đều có một cái gọi là bình yên, có khi giống nhau cũng có khi giống nhau. Nhưng lại đúc kết ra bình yêu là điều bản thân mong muốn nhất về một cuộc sống.",
+      "Thế bản chất cuộc sống là gì? Đối với tác giả, đó là cuộc đời không cô độc. Mỗi cá thể luôn sở hữu một nửa trái tim về mặt tinh thần, luôn có một nửa còn lại đang ở đâu đó trên thế gian đợi ta phát hiện. Thế cuộc sống có phải là tình yêu? Cũng có thể và cũng có thể nói tình yêu là một phần của việc được 'sống'. Ta phải làm mọi thứ dể bản thân tồn tại trên thế gian nhưng trải qua những điều để bồi dưỡng cảm xúc mới gọi là được 'sống'. Yêu và được yêu có thể nói rằng ta đã được 'sống' hơn nửa đời người.",
+      "Ta được 'sống' , được yêu, đó được gọi là một cuộc đời bình yên. Ta được là chính ta, ta có nơi quay về, đó là mái ấm, không phải là nơi chốn mà là một người ta gọi là yêu thương.",
+    ],
+    authorID: 2,
+    createdAt: new Date(),
+    img: "time.jpg",
+    likes: [1],
+  },
+  {
+    blogID: 7,
+    title: "Ta cần gì khi yêu?",
+    description:
+      "Buồn, vui, giận, hờn, dỗi, hạnh phúc, đau khổ, say đắm,... Tất cả đều có thể gói gọn trong tình yêu và chúng ta cần điều gì?",
+    contents: [
+      "Đi qua tuổi trẻ, ta tìm kiếm nơi náo nhiệt. Năng lượng thuần khiét của tuổi dậy thì thúc đẩy ta trải qua những điều ta chưa bao giờ dám. Sau khi nếm đủ hương vị của cuộc sống, những khó khăn và uất ức cũng lần lượt xuất hiện. Thế nên bình yên lại là điều cuối cùng ta muốn có được. Bình yên là gì, là cảm giác được yên lòng, là không cần bận tâm những điều xung quanh, là mỗi ngày đều không có gánh nặng trên vai, là nụ cười trên môi, là những điều mình muốn luôn xuất hiện xung quanh,... Thật ra mỗi người đều có một cái gọi là bình yên, có khi giống nhau cũng có khi giống nhau. Nhưng lại đúc kết ra bình yêu là điều bản thân mong muốn nhất về một cuộc sống.",
+      "Thế bản chất cuộc sống là gì? Đối với tác giả, đó là cuộc đời không cô độc. Mỗi cá thể luôn sở hữu một nửa trái tim về mặt tinh thần, luôn có một nửa còn lại đang ở đâu đó trên thế gian đợi ta phát hiện. Thế cuộc sống có phải là tình yêu? Cũng có thể và cũng có thể nói tình yêu là một phần của việc được 'sống'. Ta phải làm mọi thứ dể bản thân tồn tại trên thế gian nhưng trải qua những điều để bồi dưỡng cảm xúc mới gọi là được 'sống'. Yêu và được yêu có thể nói rằng ta đã được 'sống' hơn nửa đời người.",
+      "Ta được 'sống' , được yêu, đó được gọi là một cuộc đời bình yên. Ta được là chính ta, ta có nơi quay về, đó là mái ấm, không phải là nơi chốn mà là một người ta gọi là yêu thương.",
+    ],
+    authorID: 3,
+    createdAt: new Date(),
+    img: "love.jpg",
+    likes: [2, 3],
+  },
+  {
+    blogID: 8,
+    title:
+      "Người từng gặp trong đời là người đã không có cơ hội ở lại kiếp trước",
+    description:
+      "Kiếp người luôn là thứ bí ẩn, có ai biết được mình từng là ai và mình sẽ là ai và sẽ lại ai từ kiếp trước.",
+    contents: [
+      "Đi qua tuổi trẻ, ta tìm kiếm nơi náo nhiệt. Năng lượng thuần khiét của tuổi dậy thì thúc đẩy ta trải qua những điều ta chưa bao giờ dám. Sau khi nếm đủ hương vị của cuộc sống, những khó khăn và uất ức cũng lần lượt xuất hiện. Thế nên bình yên lại là điều cuối cùng ta muốn có được. Bình yên là gì, là cảm giác được yên lòng, là không cần bận tâm những điều xung quanh, là mỗi ngày đều không có gánh nặng trên vai, là nụ cười trên môi, là những điều mình muốn luôn xuất hiện xung quanh,... Thật ra mỗi người đều có một cái gọi là bình yên, có khi giống nhau cũng có khi giống nhau. Nhưng lại đúc kết ra bình yêu là điều bản thân mong muốn nhất về một cuộc sống.",
+      "Thế bản chất cuộc sống là gì? Đối với tác giả, đó là cuộc đời không cô độc. Mỗi cá thể luôn sở hữu một nửa trái tim về mặt tinh thần, luôn có một nửa còn lại đang ở đâu đó trên thế gian đợi ta phát hiện. Thế cuộc sống có phải là tình yêu? Cũng có thể và cũng có thể nói tình yêu là một phần của việc được 'sống'. Ta phải làm mọi thứ dể bản thân tồn tại trên thế gian nhưng trải qua những điều để bồi dưỡng cảm xúc mới gọi là được 'sống'. Yêu và được yêu có thể nói rằng ta đã được 'sống' hơn nửa đời người.",
+      "Ta được 'sống' , được yêu, đó được gọi là một cuộc đời bình yên. Ta được là chính ta, ta có nơi quay về, đó là mái ấm, không phải là nơi chốn mà là một người ta gọi là yêu thương.",
+    ],
+    authorID: 4,
+    createdAt: new Date(),
+    img: "mystery.jpg",
+    likes: [2, 4, 6, 3],
+  },
+  {
+    blogID: 9,
+    title: "Ánh mắt là nơi không biết nói dối",
+    description:
+      "Nơi cửa sổ tâm hồn chạm nhau, lời nói không cần nói ra cũng đã hiểu được kể cả lời yêu",
+    contents: [
+      "Đi qua tuổi trẻ, ta tìm kiếm nơi náo nhiệt. Năng lượng thuần khiét của tuổi dậy thì thúc đẩy ta trải qua những điều ta chưa bao giờ dám. Sau khi nếm đủ hương vị của cuộc sống, những khó khăn và uất ức cũng lần lượt xuất hiện. Thế nên bình yên lại là điều cuối cùng ta muốn có được. Bình yên là gì, là cảm giác được yên lòng, là không cần bận tâm những điều xung quanh, là mỗi ngày đều không có gánh nặng trên vai, là nụ cười trên môi, là những điều mình muốn luôn xuất hiện xung quanh,... Thật ra mỗi người đều có một cái gọi là bình yên, có khi giống nhau cũng có khi giống nhau. Nhưng lại đúc kết ra bình yêu là điều bản thân mong muốn nhất về một cuộc sống.",
+      "Thế bản chất cuộc sống là gì? Đối với tác giả, đó là cuộc đời không cô độc. Mỗi cá thể luôn sở hữu một nửa trái tim về mặt tinh thần, luôn có một nửa còn lại đang ở đâu đó trên thế gian đợi ta phát hiện. Thế cuộc sống có phải là tình yêu? Cũng có thể và cũng có thể nói tình yêu là một phần của việc được 'sống'. Ta phải làm mọi thứ dể bản thân tồn tại trên thế gian nhưng trải qua những điều để bồi dưỡng cảm xúc mới gọi là được 'sống'. Yêu và được yêu có thể nói rằng ta đã được 'sống' hơn nửa đời người.",
+      "Ta được 'sống' , được yêu, đó được gọi là một cuộc đời bình yên. Ta được là chính ta, ta có nơi quay về, đó là mái ấm, không phải là nơi chốn mà là một người ta gọi là yêu thương.",
+    ],
+    authorID: 7,
+    createdAt: new Date(),
+    img: "eyes.jpg",
+    likes: [1, 2, 7, 6],
+  },
+  {
+    blogID: 10,
+    title: "Do not live without smile!",
+    description: "Smile is a cure for everthing happened in the world.",
+    contents: [
+      "Đi qua tuổi trẻ, ta tìm kiếm nơi náo nhiệt. Năng lượng thuần khiét của tuổi dậy thì thúc đẩy ta trải qua những điều ta chưa bao giờ dám. Sau khi nếm đủ hương vị của cuộc sống, những khó khăn và uất ức cũng lần lượt xuất hiện. Thế nên bình yên lại là điều cuối cùng ta muốn có được. Bình yên là gì, là cảm giác được yên lòng, là không cần bận tâm những điều xung quanh, là mỗi ngày đều không có gánh nặng trên vai, là nụ cười trên môi, là những điều mình muốn luôn xuất hiện xung quanh,... Thật ra mỗi người đều có một cái gọi là bình yên, có khi giống nhau cũng có khi giống nhau. Nhưng lại đúc kết ra bình yêu là điều bản thân mong muốn nhất về một cuộc sống.",
+      "Thế bản chất cuộc sống là gì? Đối với tác giả, đó là cuộc đời không cô độc. Mỗi cá thể luôn sở hữu một nửa trái tim về mặt tinh thần, luôn có một nửa còn lại đang ở đâu đó trên thế gian đợi ta phát hiện. Thế cuộc sống có phải là tình yêu? Cũng có thể và cũng có thể nói tình yêu là một phần của việc được 'sống'. Ta phải làm mọi thứ dể bản thân tồn tại trên thế gian nhưng trải qua những điều để bồi dưỡng cảm xúc mới gọi là được 'sống'. Yêu và được yêu có thể nói rằng ta đã được 'sống' hơn nửa đời người.",
+      "Ta được 'sống' , được yêu, đó được gọi là một cuộc đời bình yên. Ta được là chính ta, ta có nơi quay về, đó là mái ấm, không phải là nơi chốn mà là một người ta gọi là yêu thương.",
+    ],
+    authorID: 5,
+    createdAt: new Date(),
+    img: "eyes.jpg",
+    likes: [1, 3, 4],
   },
 ]);
 
